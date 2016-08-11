51e18685c85b48bc80a5a2be4e4b025cc069f946
Matthew Hall <matthew.hall@imperial.ac.uk>
2016-08-11 15:05:13 +0100

    Now outputs a list of tips, not patients

--- MakePatientBlacklist.R
+++ MakePatientBlacklist.R
@@ -1,35 +1,50 @@
+library(ape)
+
 args = commandArgs(trailingOnly=TRUE)
+
+patient.from.label <- function(label){
+  return(unlist(strsplit(label, "_read_"))[1])
+}
 
 # This script blacklists all reads from all patients in a given window. It is currently very 
 # BEEHIVE specific (unlike MakeReadBlacklist.R) and it would be suggested that other uses
-# simply provide custom read blacklists
+# simply provide custom read blacklists.
 
 start <- as.numeric(args[1])
 end <- as.numeric(args[2])
-in.root <- args[3]
-out.root <- args[4]
+in.file <- args[3]
+tree.root <- args[4]
+out.root <- args[5]
 
 boundaries <- c(800, 1505, 2385, 4805, 5037, 5985, 7831, 9400)
 
-amps <- read.table(in.root, sep=",", header=T, stringsAsFactors = F)
+amps <- read.table(in.file, sep=",", header=T, stringsAsFactors = F)
+tree <- read.tree(paste(in.root,"_",start,"_to_",end,".tree", sep=""))
+
+bad.patients <- vector()
 
 for(row in seq(1,nrow(amps))){
   if(amps[row,2]){
-    out <- rbind(amps[row,1])
+    bad.patients <- c(bad.patients, amps[row,1])
   } else {
     for(amp.id in seq(1,7)){
       start.amp.section <- boundaries[amp.id]
       end.amp.section <- boundaries[amp.id+1]
       if(!amps[row,2+amp.id] & ((start>start.amp.section & start<end.amp.section) | (end>start.amp.section & end < end.amp.section))){
-        out <- rbind(out, amps[row,1])
+        bad.patients <- c(bad.patients, amps[row,1])
       }
     }
   }
 }
 
-cat("Blacklist length: ",length(out),"\n",sep="")
+tip.is.bad <- sapply(tree$tip.label, function(x)  patient.from.label(x) %in% bad.patients)
 
-if(length(out)>0){
-  write.table(out,output.name, sep=",", row.names=FALSE, col.names=FALSE)
+bad.tips <- tree$tip.label[which(tip.is.bad)]
+
+
+cat("Blacklist length: ",length(bad.tips),"\n",sep="")
+
+if(length(bad.tips)>0){
+  write.table(bad.tips,output.name, sep=",", row.names=FALSE, col.names=FALSE)
 }
 }
