26dca9e188b57d96c583f5a7f5f17b19fad3e121
Matthew Hall <matthew.hall@imperial.ac.uk>
2016-11-21 15:38:00 +0000

    Output is now tailored to be readable by read.beast from ggtree (NEXUS formats being rather variable)

--- tools/WriteAnnotatedTrees.R
+++ tools/WriteAnnotatedTrees.R
@@ -167,20 +167,20 @@ write.ann.nexus <- function(..., annotations = NULL, file = "", translate = TRUE
   
   N <- length(obj[[1]]$tip.label)
   
-  cat("BEGIN TAXA;\n", file = file, append = TRUE)
+  cat("Begin TAXA;\n", file = file, append = TRUE)
   cat(paste("\tDIMENSIONS NTAX = ", N, ";\n", sep = ""),
       file = file, append = TRUE)
   cat("\tTAXLABELS\n", file = file, append = TRUE)
-  cat(paste("\t\t", obj[[1]]$tip.label, sep = ""),
+  cat(paste("\t\t'", obj[[1]]$tip.label, "'", sep = ""),
       sep = "\n", file = file, append = TRUE)
   cat("\t;\n", file = file, append = TRUE)
-  cat("END;\n", file = file, append = TRUE)
+  cat("End;\n", file = file, append = TRUE)
   
-  cat("BEGIN TREES;\n", file = file, append = TRUE)
+  cat("Begin trees;\n", file = file, append = TRUE)
   if (translate) {
     cat("\tTRANSLATE\n", file = file, append = TRUE)
     obj <- .compressTipLabel(obj)
-    X <- paste("\t\t", 1:N, "\t", attr(obj, "TipLabel"), ",", sep = "")
+    X <- paste("\t\t", 1:N, " '", attr(obj, "TipLabel"), "',", sep = "")
     ## We remove the last comma:
     X[length(X)] <- gsub(",", "", X[length(X)])
     cat(X, file = file, append = TRUE, sep = "\n")
@@ -208,9 +208,9 @@ write.ann.nexus <- function(..., annotations = NULL, file = "", translate = TRUE
   for (i in 1:ntree) {
     if (class(obj[[i]]) != "phylo") next
     root.tag <- if (is.rooted(obj[[i]])) "= [&R] " else "= [&U] "
-    cat("\tTREE *", title[i], root.tag, file = file, append = TRUE)
+    cat("\tTree *", title[i], root.tag, file = file, append = TRUE)
     cat(write.ann.tree(obj[[i]], annotations = annotations, file = ""),
         "\n", sep = "", file = file, append = TRUE)
   }
-  cat("END;\n", file = file, append = TRUE)
+  cat("End;\n", file = file, append = TRUE)
 }
