c36777147e5057059b5da1b180d2cb8b112e11dc
Matthew Hall <matthew.hall@imperial.ac.uk>
2017-03-14 13:51:16 +0000

    Code cleaned and commented

--- tools/ParsimonyBasedBlacklister.R
+++ tools/ParsimonyBasedBlacklister.R
@@ -5,8 +5,9 @@ if(length(new.packages)) install.packages(new.packages, dependencies = T, repos=
 suppressMessages(library(argparse, quietly=TRUE, warn.conflicts=FALSE))
 
 command.line <- T
-
 if(command.line){
+  # Define arguments
+  
   arg_parser = ArgumentParser(description="Identify phylogeny tips for blacklisting as representing suspected contaminants, based on a Sankhoff parsimony reconstruction")  
   arg_parser$add_argument("-x", "--tipRegex", action="store", default="^(.*)_read_([0-9]+)_count_([0-9]+)$", help="Regular expression identifying tips from the dataset. Three groups: patient ID, read ID, and read count. If absent, input will be assumed to be from the phyloscanner pipeline, and the patient ID will be the BAM file name.")
   arg_parser$add_argument("-D", "--scriptdir", action="store", help="Full path of the script directory.", default="/Users/twoseventwo/Documents/phylotypes/")
@@ -20,6 +21,7 @@ if(command.line){
   arg_parser$add_argument("sankhoffK", action="store", type="double", help="The k parameter in the cost matrix for Sankhoff reconstruction (see documentation)")
   arg_parser$add_argument("inputFileName", action="store", help="A CSV file outlining groups of tips that have identical sequences, each forming a single line.")
   arg_parser$add_argument("blacklistOutputFileName", action="store", help="The file to write a list of tips to be blacklisted to.")
+  
   # Parse arguments
   args <- arg_parser$parse_args()
   raw.threshold <- args$rawThreshold
@@ -36,27 +38,143 @@ if(command.line){
   verbose <- args$verbose
   
 } else {
-  setwd("/Users/twoseventwo/Dropbox (Infectious Disease)/BEEHIVE/phylotypes/run20161013/")
-  script.dir <- "/Users/twoseventwo/Documents/phylotypes/tools/"
-  
-  input.name <- "AllTrees/RAxML_bestTree.InWindow_2300_to_2650.tree"
-  
-  #anything already blacklisted
-  blacklist.file.name <- "Blacklists/AmpliconBlacklist_InWindow_2300_to_2650.csv"
-  
-  root.name <- "C.BW.00.00BW07621.AF443088"
-  tip.regex <- "^(.*)-[0-9].*_read_([0-9]+)_count_([0-9]+)$"
-  
-  sankhoff.k <- 20
-  
-  # Threshold for propotion of reads that come from a patient to be in a split for it to be considered a dual infection, not a
-  # rogue
-  
-  raw.threshold <- 3
-  ratio.threshold <- 0.005
-  
 
 }
+
+# The main function for getting splits for a given patient ID
+
+get.splits.for.patient <- function(patient, tip.patients, tree, root.name, raw.threshold, ratio.threshold, d.output.name, verbose){
+  cat("Identifying splits for patient ", patient, "\n")
+  
+  blacklist.items <- vector()
+  
+  dual <- F
+  
+  if(length(which(tip.patients==patient))>1){
+    
+    # Keep only the tips from this patient and the outgroup
+    
+    outgroup.no <- which(tree$tip.label==root.name)
+    tip.nos <- c(outgroup.no, which(tip.patients==patient))
+    
+    subtree <- drop.tip(tree, setdiff(1:length(tree$tip.label), tip.nos))
+    
+    # Collapse multifurcations
+    
+    subtree <- di2multi(subtree, tol = 1E-5)
+    
+    # Re-find the root
+    
+    st.outgroup.no <- which(subtree$tip.label==root.name)
+    
+    # Set up split.and.annotate (which takes a list)
+    
+    patient.tips <- list()
+    patient.tips[[patient]] <- setdiff(1:length(subtree$tip.label), st.outgroup.no)
+    
+    # Perform split.and.annotate; get a list of splits
+    
+    split.results <- split.and.annotate(subtree, patient, patient.tips, NULL, NULL, tip.regex, "s", sankhoff.k, "u", sankhoff.mode = "total.lengths", useff = F)
+    
+    # vector of of split IDs
+    
+    patient.split.ids <- split.results$split.patients
+    
+    # list of tips that belong to each split ID
+    
+    tips.for.splits <- split.results$split.tips
+    
+    # The total number of reads is either got from the tips or assumed to be one per tip
+    
+    if(!no.read.counts){
+      reads.per.tip <- sapply(subtree$tip.label, function(x) read.count.from.label(x, tip.regex))
+    } else {
+      reads.per.tip <- rep(1, length(subtree$tip.label))
+      reads.per.tip[root.no] <- NA
+    }
+    
+    total.reads <- sum(reads.per.tip[!is.na(reads.per.tip)])
+    
+    if(length(patient.split.ids)==1){
+      
+      # if the algorithm didn't split the tips
+      
+      if(verbose){
+        cat("No splits for patient ", patient, "\n", sep="")
+      }
+      if(total.reads < raw.threshold){
+        # blacklist the whole patient if the read count from its only subgraph is below the raw threshold
+        
+        if(verbose){
+          cat("Blacklisting ",patient,"; not enough reads in total.\n", sep="")
+        }
+        blacklist.items <- subtree$tip.label[which(subtree$tip.label!=root.name)]
+      }
+      
+    } else {
+      
+      # at least two subgraphs
+      
+      cat(length(patient.split.ids), " splits for patient ", patient, "\n", sep="")
+      
+      props <- vector()
+      too.small <- vector()
+      
+      tips.vector <- vector()
+      read.count.vector <- vector()
+      tip.count.vector <- vector()
+      
+      too.small <- sapply(patient.split.ids, function(x) check.read.count.for.split(x, tips.for.splits, raw.threshold, ratio.threshold, reads.per.tip, total.reads))
+
+      if(length(which(!too.small))>1 & !is.null(d.output.name)){
+        # there are at least two subgraphs which are too big to be contaminants
+        
+        if(verbose){
+          cat(patient, " looks like a dual infection\n", sep="")
+        }
+        
+        dual <- T
+      }
+      
+      # blacklist the tips from small subgraphs
+      
+      if(length(which(too.small))>0){
+        blacklist.items <- subtree$tip.label[unlist(tips.for.splits[patient.split.ids[which(too.small)]])]
+      }
+      
+    }
+    
+    tips.vector <- subtree$tip.label[unlist(tips.for.splits[patient.split.ids])]
+    read.count.vector <- unlist(lapply(patient.split.ids, function(x){
+      rep(sum(read.count.from.label(subtree$tip.label[tips.for.splits[[x]]], tip.regex)), length(tips.for.splits[[x]]))
+    }))
+    tip.count.vector <- unlist(lapply(patient.split.ids, function(x){
+      rep(length(tips.for.splits[[x]]), length(tips.for.splits[[x]]))
+    }))
+  } else {
+    # this covers the situation where there is only one patient tip (because ape has trouble with two-tip trees)
+    
+    tip.no <- which(tip.patients==patient)
+    
+    reads <- read.count.from.label(tree$tip.label[tip.no], tip.regex)
+    
+    if(reads < raw.threshold){
+      if(verbose){
+        cat("Blacklisting ",patient,"; not enough reads in total.\n", sep="")
+      }
+      blacklist.items <- tree$tip.label[tip.no]
+    }
+    
+    tips.vector <- tree$tip.label[tip.no]
+    read.count.vector <- reads
+    tip.count.vector <- 1
+  }
+
+  
+  list(id = patient, blacklist.items = blacklist.items, tip.names = tips.vector, read.counts = read.count.vector, tip.counts = tip.count.vector, dual=dual)
+}
+
+# Function that returns the third group from a regex as a numeric (the read count from a tip label)
 
 get.count <- function(string){
   if(length(grep(tip.regex, string)>0)) {
@@ -66,21 +184,41 @@ get.count <- function(string){
   }
 }
 
+# Return whether the number of reads in this split is below one of the thresholds
+
+check.read.count.for.split <- function(split, tips.for.splits, raw.threshold, ratio.threshold, reads.per.tip, total.reads){
+  # get the read counts
+  
+  count.in.split <- sum(reads.per.tip[tips.for.splits[[split]]])
+  
+  # find what proportion of all reads from this patient are in this subgraph and check against the thresholds
+  
+  prop.in.split <- count.in.split/total.reads
+  props <- c(props, prop.in.split)
+  return (count.in.split < raw.threshold | prop.in.split < ratio.threshold)
+}
+
 cat("Reading functions...\n")
+
 source(file.path(script.dir, "TreeUtilityFunctions.R"))
 source(file.path(script.dir, "ParsimonyReconstructionMethods.R"))
 
 cat(paste("Reading tree (",input.name,")...\n",sep=""))
 
 tree <- read.tree(input.name)
+
+# Re-root the tree
+
 tree <- unroot(tree)
-outgroup.no <- which(tree$tip.label==root.name)
 
 if(!is.null(root.name)){
+  outgroup.no <- which(tree$tip.label==root.name)
   tree <- root(tree, outgroup = outgroup.no, resolve.root = T)
 }
 
 tip.labels <- tree$tip.label
+
+# Import existing blacklist
 
 blacklist <- vector()
 
@@ -98,10 +236,10 @@ if(!is.null(blacklist.file.name)){
 
 cat("Collecting tips for each patient...\n")
 
-patient.ids <- sapply(tip.labels, function(x) patient.from.label(x, tip.regex))
-patient.ids[blacklist] <- NA
+tip.patients <- sapply(tip.labels, function(x) patient.from.label(x, tip.regex))
+tip.patients[blacklist] <- NA
 
-patients <- unique(na.omit(patient.ids))
+patients <- unique(na.omit(tip.patients))
 
 new.blacklist <- tree$tip.label[blacklist]
 
@@ -110,105 +248,35 @@ mi.tip.names.column <- vector()
 mi.read.count.column <- vector()
 mi.tip.count.column <- vector()
 
-for(patient in patients[order(patients)]){
-  cat("Identifying splits for patient ", patient, "\n")
+# Re-order the patients (mostly for the sake of getting a sense of progress in screen output)
 
-  if(length(which(patient.ids==patient))>1){
-    tip.nos <- c(outgroup.no, which(patient.ids==patient))
-    
-    subtree <- drop.tip(tree, setdiff(1:length(tree$tip.label), tip.nos))
-    subtree <- di2multi(subtree, tol = 1E-5)
-    
-    root.no <- which(subtree$tip.label==root.name)
-    
-    patient.tips <- list()
-    patient.tips[[patient]] <- setdiff(1:length(subtree$tip.label), root.no)
-    
-    split.results <- split.and.annotate(subtree, patient, patient.tips, NULL, NULL, tip.regex, "s", sankhoff.k, "u", sankhoff.mode = "total.lengths", useff = F)
-    
-    if(!no.read.counts){
-      total.reads <- sum(sapply(subtree$tip.label[setdiff(1:length(subtree$tip.label), root.no)], function(x) read.count.from.label(x, tip.regex)))
-    } else {
-      total.reads <- length(subtree$tip.label)
-    }
-    
-    if(length(split.results$split.patients)==1){
-      if(verbose){
-        cat("No splits for patient ", patient, "\n", sep="")
-      }
-      if(total.reads < raw.threshold){
-        if(verbose){
-          cat("Blacklisting ",patient,"; not enough reads in total.\n", sep="")
-        }
-        new.blacklist <- c(new.blacklist, subtree$tip.label[which(subtree$tip.label!=root.name)])
-      }
-      
-    } else {
-      
-      cat(length(split.results$split.patients), " splits for patient ", patient, "\n", sep="")
-      
-      props <- vector()
-      too.small <- vector()
-      
-      tips.vector <- vector()
-      read.count.vector <- vector()
-      tip.count.vector <- vector()
-      
-      for(split in split.results$split.patients){
-        if(!no.read.counts){
-          count.in.split <- sum(sapply(subtree$tip.label[split.results$split.tips[[split]]], function(x) read.count.from.label(x, tip.regex)))
-        } else {
-          count.in.split <- length(subtree$tip.label[split.results$split.tips[[split]]])
-        }
-        prop.in.split <- count.in.split/total.reads
-        props <- c(props, prop.in.split)
-        too.small <- c(too.small, count.in.split < raw.threshold | prop.in.split < ratio.threshold)
-        
-        tips.vector <- c(tips.vector, subtree$tip.label[split.results$split.tips[[split]]])
-        read.count.vector <- c(read.count.vector, rep(count.in.split, length(split.results$split.tips[[split]])))
-        tip.count.vector <- c(tip.count.vector, rep(length(split.results$split.tips[[split]]), length(split.results$split.tips[[split]])))
-        
-      }
-      if(length(which(!too.small))>1 & !is.null(d.output.name)){
-        if(verbose){
-          cat(patient, " looks like a dual infection\n", sep="")
-        }
-        mi.patient.column <- c(mi.patient.column, rep(patient, length(subtree$tip.label)-1))
-        mi.tip.names.column <- c(mi.tip.names.column, tips.vector)
-        mi.read.count.column <- c(mi.read.count.column, read.count.vector)
-        mi.tip.count.column <- c(mi.tip.count.column, tip.count.vector)
-        
-      }
-      for(small.group in which(too.small)){
-        cat("Tips from ", split.results$split.patients[small.group], " blacklisted\n", sep="")
-        new.blacklist <- c(new.blacklist, subtree$tip.label[split.results$split.tips[[split.results$split.patients[small.group]]]])
-      }
-    }
-  } else {
-    tip.no <- which(patient.ids==patient)
-    
-    reads <- read.count.from.label(tree$tip.label[tip.no], tip.regex)
-    
-    if(reads < raw.threshold){
-      if(verbose){
-        cat("Blacklisting ",patient,"; not enough reads in total.\n", sep="")
-      }
-      new.blacklist <- c(new.blacklist, tree$tip.label[tip.no])
-    }
-    
-  }
-}
+patients <- patients[order(patients)]
+
+results <- lapply(patients, function(x) get.splits.for.patient(x, tip.patients, tree, root.name, raw.threshold, ratio.threshold, d.output.name, verbose))
+
 cat("Finished\n")
+
+new.blacklist <- unlist(lapply(results, "[[", 2))
+
+# Write the output
 
 write.table(new.blacklist, b.output.name, sep=",", row.names=FALSE, col.names=FALSE, quote=F)
 
 if(!is.null(d.output.name)){
-  if(length(mi.patient.column>0)){
-    mi.df <- data.frame(patient = mi.patient.column, tip.name = mi.tip.names.column, reads.in.subtree = mi.read.count.column,
-                        tips.in.subtree = mi.tip.count.column)
+  
+  which.are.duals <- which(unlist(lapply(results, "[[", 6)))
+  
+  if(length(which.are.duals) > 0) {
+    mi.df <- data.frame(patient = unlist(sapply(results[which.are.duals], function (x) rep(x$id, length(x$tip.names)) )), 
+                        tip.name = unlist(lapply(results[which.are.duals], "[[", 3)),
+                        reads.in.subtree = unlist(lapply(results[which.are.duals], "[[", 4)),
+                        tips.in.subtree = unlist(lapply(results[which.are.duals], "[[", 5))
+    )
     
     write.csv(mi.df, d.output.name, row.names = F)
   } else {
+    # Other scripts behave better when these files exist even if they are empty
+    
     cat("No multiple infections detected; writing empty file.\n")
     file.create(d.output.name)
   }
