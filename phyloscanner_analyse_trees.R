6165757812e8ff1b1e78bebd6274c7721b7e4f15
Matthew Hall <whgu0705@nexus.ox.ac.uk>
2017-06-28 10:27:32 +0100

    Renamed

--- phyloscanner_analyse_trees.R
+++ phyloscanner_analyse_trees.R
@@ -26,7 +26,7 @@ suppressMessages(require(kimisc, quietly=TRUE, warn.conflicts=FALSE))
 arg_parser		     <- ArgumentParser()
 
 arg_parser$add_argument("tree", action="store", help="A path and string that begins all the tree file names.")
-arg_parser$add_argument("output.string", action="store", help="This string identifies all output files.")
+arg_parser$add_argument("outputString", action="store", help="This string identifies all output files.")
 
 arg_parser$add_argument("-r", "--outgroupName", action="store", help="Label of tip to be used as outgroup (if unspecified, tree will be assumed to be already rooted).")
 arg_parser$add_argument("-m", "--multifurcationThreshold", help="If specified, short branches in the input tree will be collapsed to form multifurcating internal nodes. This is recommended; many phylogenetics packages output binary trees with short or zero-length branches indicating multifurcations. If a number, this number will be used as the threshold, with all branches strictly smaller collapsed. If 'g', it will be guessed from the branch lengths (use this only if you have checked by eye that the tree does indeed have multifurcations).")
