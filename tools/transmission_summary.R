08a0b6d7921c9c0ab54e81f0641e4057977a4148
Matthew Hall <matthew.hall@bdi.ox.ac.uk>
2017-06-30 18:47:28 +0100

    The last of the individual scripts is checked

--- tools/transmission_summary.R
+++ tools/transmission_summary.R
@@ -1,30 +1,43 @@
 #!/usr/bin/env Rscript
 
-list.of.packages <- c("prodlim","reshape2")
+list.of.packages <- c("prodlim", "reshape2", "kimisc")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies = TRUE, repos="http://cran.ma.imperial.ac.uk/")
+
+suppressMessages(library(prodlim, quietly=TRUE, warn.conflicts=FALSE))
+suppressMessages(library(reshape2, quietly=TRUE, warn.conflicts=FALSE))
+suppressMessages(library(gdata, quietly=TRUE, warn.conflicts=FALSE))
+suppressMessages(library(ggplot2, quietly=TRUE, warn.conflicts=FALSE))
+suppressMessages(require(data.table, quietly=TRUE, warn.conflicts=FALSE))
+suppressMessages(require(kimisc, quietly=TRUE, warn.conflicts=FALSE))
 
 suppressMessages(library(argparse, quietly=TRUE, warn.conflicts=FALSE))
 tmp	<- "Summarise topological relationships suggesting direction of transmission across windows. Outputs a .csv file of relationships between patient IDs. Relationship types: 'anc' indicates a topology that suggests the patient in column 1 infected the patient in column 2, 'cher' where there is only one subtree per patient and the MRCAs of both have the same parent in the phylogeny, 'unint' where cher is not present reads from the patients form sibling clades from an unsampled common ancestor patient (and that neither has any sampled ancestors occuring later in the transmission chain than that common ancestor), 'int' one where reads from both patients are intermingled and the direction of transmission cannot be established. (More documentation to follow.)"
 arg_parser = ArgumentParser(description=tmp)
-arg_parser$add_argument("-m", "--minThreshold", action="store", default=1, type="integer", help="Relationships between two patients will only appear in output if they are within the distance threshold and ajacent to each other least these many windows (default 1). High numbers are useful for drawing figures in e.g. Cytoscape with few enough arrows to be comprehensible. The script is extremely slow if this is set to 0.")
+arg_parser$add_argument("-m", "--minThreshold", action="store", default=0, type="double", help="Relationships between two patients will only appear in output if they are within the distance threshold and ajacent to each other in more than this proportion of trees many tree (default 0). High numbers are useful for drawing figures in e.g. Cytoscape with few enough arrows to be comprehensible.")
 arg_parser$add_argument("-c", "--distanceThreshold", action="store", default=-1, help="Maximum distance threshold on a window for a relationship to be reconstructed between two patients on that window.")
 arg_parser$add_argument("-p", "--allowMultiTrans", action="store_true", default=FALSE, help="If absent, directionality is only inferred between pairs of patients where a single clade from one patient is nested in one from the other; this is more conservative")
 arg_parser$add_argument("-cfe", "--csvFileExtension", action="store", default="csv", help="The file extension for table files (default .csv).")
 arg_parser$add_argument("idFile", action="store", help="A file containing a list of the IDs of all the patients to calculate and display statistics for.")
 arg_parser$add_argument("inputFiles", action="store", help="Either (if -l is present) a list of all input files (output from ClassifyRelationships.R), separated by colons, or (if not) a single string that begins every input file name.")
 arg_parser$add_argument("outputFile", action="store", help="A .csv file to write the output to.")
-arg_parser$add_argument("-D", "--scriptdir", action="store", help="Full path of the script directory.", default="/Users/twoseventwo/Documents/phylotypes/")
+arg_parser$add_argument("-D", "--scriptDir", action="store", help="Full path of the /tools directory.")
 arg_parser$add_argument("-v", "--verbose", action="store_true", default=FALSE, help="Talk about what I'm doing.")
-
 
 args <- arg_parser$parse_args()
 
+if(!is.null(args$scriptDir)){
+  script.dir          <- args$scriptDir
+} else {
+  script.dir          <- dirname(thisfile())
+  if(!dir.exists(script.dir)){
+    stop("Cannot detect the location of the /phyloscanner/tools directory. Please specify it at the command line with -D.")
+  }
+}
 
 summary.file             <- args$summaryFile
 id.file                  <- args$idFile
 output.file              <- args$outputFile
-script.dir               <- args$scriptdir  
 verbose                  <- args$verbose
 csv.fe                   <- args$csvFileExtension
 min.threshold            <- as.numeric(args$minThreshold)
@@ -39,16 +52,9 @@ if(is.null(min.threshold)){
 allow.mt                 <- args$allowMultiTrans
 input.file.name          <- args$inputFiles
 
-source(file.path(script.dir, "TreeUtilityFunctions.R"))
-source(file.path(script.dir, "GeneralFunctions.R"))
-source(file.path(script.dir, "CollapsedTreeMethods2.R"))
-
-suppressMessages(library(prodlim, quietly=TRUE, warn.conflicts=FALSE))
-suppressMessages(library(reshape2, quietly=TRUE, warn.conflicts=FALSE))
-suppressMessages(library(gdata, quietly=TRUE, warn.conflicts=FALSE))
-suppressMessages(library(ggplot2, quietly=TRUE, warn.conflicts=FALSE))
-suppressMessages(require(data.table, quietly=TRUE, warn.conflicts=FALSE))
-
+source(file.path(script.dir, "tree_utility_functions.R"))
+source(file.path(script.dir, "general_functions.R"))
+source(file.path(script.dir, "collapsed_tree_methods.R"))
 
 input.files <- list.files.mod(dirname(input.file.name), pattern=paste(basename(input.file.name)), full.names=TRUE)
 
@@ -78,8 +84,7 @@ for(file in input.files){
   all.tree.info[[file]] <- tree.info
 }
 
-
-results <- summarise.classifications(all.tree.info, patient.ids, min.threshold, dist.threshold, allow.mt, csv.fe, verbose)
+results <- summarise.classifications(all.tree.info, patient.ids, min.threshold*length(all.tree.info), dist.threshold, allow.mt, csv.fe, verbose)
 
 if (verbose) cat('Writing summary to file',output.file,'\n')
 write.csv(results, file=output.file, row.names=FALSE, quote=FALSE)
