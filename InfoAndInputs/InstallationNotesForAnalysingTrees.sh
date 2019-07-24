cbd743d736a7ae95f4ea2ce6ef1e1c9a026adc4d
ChrisHIV <chris.wymant@bdi.ox.ac.uk>
2019-07-24 11:29:47 +0100

    Update InstallationNotesForAnalysingTrees.sh

--- InfoAndInputs/InstallationNotesForAnalysingTrees.sh
+++ InfoAndInputs/InstallationNotesForAnalysingTrees.sh
@@ -5,26 +5,12 @@
 
 ############################################################
 # INSTALLING UP-TO-DATE R ON UBUNTU
-# The comands below *should* do it.
-
-# Add an R repository to the list of things Ubuntu should keep up to date.
-# The command below uses the R studio mirror (you can use others), and specifies 'trusty' as the
-# codename for the Ubuntu version you're running (you should replace this appropriately if running
-# a different version of Ubuntu). See https://cran.r-project.org/bin/linux/ubuntu/README.html for
-# details.
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
-
-# Gather information about what needs updating:
-sudo apt-get update
-# That command may fail with an error message saying "GPG error",
-# "the public key is not available", "The repository... is not signed".
-# In that case, try adding the key thus:
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-# That may fail, for example due to firewall issues. In that case, consult the section
+# Following the steps at https://www.r-bloggers.com/updating-r-on-ubuntu/
+# worked for me.
+# However, the command 
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+# may fail, for example due to firewall issues. In that case, consult the section
 # 'Secure APT' at https://cran.r-project.org/bin/linux/ubuntu/README.html.
-# Once you've managed to add the key, rerun the 'sudo apt-get update' command. Then,
-sudo apt-get install r-base
-sudo apt-get install r-base-dev
 ############################################################
 
 ############################################################
