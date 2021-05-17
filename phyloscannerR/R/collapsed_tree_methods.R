a3600e2c492d07a3b9bcbbd45558a38ab7f14a20
Xiaoyue Xi <xiaoyue.xi94@gmail.com>
2021-05-17 16:23:27 +0100

    fix multitrans

--- phyloscannerR/R/collapsed_tree_methods.R
+++ phyloscannerR/R/collapsed_tree_methods.R
@@ -756,6 +756,9 @@ classify <- function(ptree, allow.mt = F, relaxed.ancestry = F, verbose = F, no.
   dir.21.matrix <- matrix(NA, length(hosts.included), length(hosts.included), dimnames=list(hosts.included, hosts.included))
   min.distance.matrix <- matrix(NA, length(hosts.included), length(hosts.included), dimnames=list(hosts.included, hosts.included))
   
+  
+  n.mt <- 3
+  
   if(total.host.pairs==0 & verbose & !no.progress.bars){
     setTxtProgressBar(progress.bar, 1)
   } else {
@@ -810,7 +813,11 @@ classify <- function(ptree, allow.mt = F, relaxed.ancestry = F, verbose = F, no.
               top.class.matrix[pat.1, pat.2] <- "anc"
             } else {
               if(allow.mt){
-                top.class.matrix[pat.1, pat.2] <- "multiAnc"
+                if(count.12>=n.mt){
+                  top.class.matrix[pat.1, pat.2] <- "complex"
+                }else{
+                  top.class.matrix[pat.1, pat.2] <- "multiAnc"  
+                }
               } else {
                 top.class.matrix[pat.1, pat.2] <- "complex"
               }
@@ -820,7 +827,11 @@ classify <- function(ptree, allow.mt = F, relaxed.ancestry = F, verbose = F, no.
               top.class.matrix[pat.1, pat.2] <- "desc"
             } else {
               if(allow.mt){
-                top.class.matrix[pat.1, pat.2] <- "multiDesc"
+                if(count.21>=n.mt){
+                  top.class.matrix[pat.1, pat.2] <- "complex"
+                }else{
+                  top.class.matrix[pat.1, pat.2] <- "multiDesc"  
+                }
               } else {
                 top.class.matrix[pat.1, pat.2] <- "complex"
               }
