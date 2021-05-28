580e7429c70a9a9a0f5d2fa46684a0c6bd7e8733
Xiaoyue Xi <xiaoyue.xi94@gmail.com>
2021-05-28 11:46:45 +0100

    minor fix

--- phyloscannerR/R/collapsed_tree_methods.R
+++ phyloscannerR/R/collapsed_tree_methods.R
@@ -803,11 +803,74 @@ classify <- function(ptree, allow.mt = F, n.mt=Inf, p.mt= 2, relaxed.ancestry = 
           dir.12.matrix[pat.1, pat.2] <- count.12
           dir.21.matrix[pat.1, pat.2] <- count.21
           
-          tmp <- grep('^AID[0-9]+-fq[0-9]+_read_[0-9]+_count_[0-9]+$',tree$tip.label,value = T)
-          count.tip.1 <- length(grep(pat.1.id,tmp,value=T))
-          count.tip.2 <- length(grep(pat.2.id,tmp,value=T))
-          p.12 <- count.12/count.tip.2
-          p.21 <- count.21/count.tip.1
+          tips.1 <- grep(pat.1.id,tree$tip.label, value=T)
+          tips.2 <- grep(pat.2.id,tree$tip.label, value=T)
+          tips.no.1 <- sapply(tips.1,function(tip){get.tip.no(tree, tip)})
+          tips.no.2 <- sapply(tips.2,function(tip){get.tip.no(tree, tip)})
+          tips.mrca.1 <- mrca.phylo(tree,tips.no.1)
+          tips.mrca.2 <- mrca.phylo(tree,tips.no.2)
+          
+          # tmp <- grep('^AID[0-9]+-fq[0-9]+_read_[0-9]+_count_[0-9]+$',tree$tip.label,value = T)
+          # count.tip.1 <- length(grep(pat.1.id,tmp,value=T))
+          # count.tip.2 <- length(grep(pat.2.id,tmp,value=T))
+          # p.12 <- count.12/count.tip.2
+          # p.21 <- count.21/count.tip.1
+          # 
+          # if(n.mt<=1e5 & p.mt>1){warning('n.mt and p.mt were all specified. the classification is based on p.mt.')}
+          # if(count.12 == 0 & count.21 == 0){
+          #   top.class.matrix[pat.1, pat.2] <- "noAncestry"
+          # } else if(count.12 != 0 & count.21 == 0 & (relaxed.ancestry | prop.12 == 1)) {
+          #   if(count.12 == 1){
+          #     top.class.matrix[pat.1, pat.2] <- "anc"
+          #   } else {
+          #     if(allow.mt){
+          #       if(p.mt<=1){
+          #         if(p.12 >=p.mt){
+          #           top.class.matrix[pat.1, pat.2] <- "complex"
+          #         }else{
+          #           top.class.matrix[pat.1, pat.2] <- "multiAnc"  
+          #         }
+          #         
+          #       }else if(n.mt<=1e5){
+          #         if(count.12>=n.mt){
+          #           top.class.matrix[pat.1, pat.2] <- "complex"
+          #         }else{
+          #           top.class.matrix[pat.1, pat.2] <- "multiAnc"  
+          #         }
+          #       }else{
+          #         top.class.matrix[pat.1, pat.2] <- "multiAnc"  
+          #       }
+          #     } else {
+          #       top.class.matrix[pat.1, pat.2] <- "complex"
+          #     }
+          #   }
+          # } else if(count.21 != 0 & count.12 == 0 & (relaxed.ancestry | prop.21 == 1)) {
+          #   if(count.21 == 1){
+          #     top.class.matrix[pat.1, pat.2] <- "desc"
+          #   } else {
+          #     if(allow.mt){
+          #       if(p.mt<=1){
+          #         if(p.21 >=p.mt){
+          #           top.class.matrix[pat.1, pat.2] <- "complex"
+          #         }else{
+          #           top.class.matrix[pat.1, pat.2] <- "multiDesc"  
+          #         }
+          #       }else if(n.mt<=1e5){
+          #         if(count.21>=n.mt){
+          #           top.class.matrix[pat.1, pat.2] <- "complex"
+          #         }else{
+          #           top.class.matrix[pat.1, pat.2] <- "multiDesc"  
+          #         }
+          #       }else{
+          #         top.class.matrix[pat.1, pat.2] <- "multiDesc"  
+          #       }
+          #     } else {
+          #       top.class.matrix[pat.1, pat.2] <- "complex"
+          #     }
+          #   }
+          # } else {
+          #   top.class.matrix[pat.1, pat.2] <- "complex"
+          # }
           
           if(count.12 == 0 & count.21 == 0){
             top.class.matrix[pat.1, pat.2] <- "noAncestry"
@@ -815,22 +878,10 @@ classify <- function(ptree, allow.mt = F, n.mt=Inf, p.mt= 2, relaxed.ancestry = 
             if(count.12 == 1){
               top.class.matrix[pat.1, pat.2] <- "anc"
             } else {
-              if(allow.mt){
-                if(p.mt<=1){
-                  if(p.12 >=p.mt){
-                    top.class.matrix[pat.1, pat.2] <- "complex"
-                  }else{
-                    top.class.matrix[pat.1, pat.2] <- "multiAnc"  
-                  }
-                  if(n.mt<=1e5){warning('n.mt and p.mt were all specified. the classification is based on p.mt.')}
-                }else if(n.mt<=1e5){
-                  if(count.12>=n.mt){
-                    top.class.matrix[pat.1, pat.2] <- "complex"
-                  }else{
-                    top.class.matrix[pat.1, pat.2] <- "multiAnc"  
-                  }
+              if(tips.mrca.1==tips.mrca.2){
+                  top.class.matrix[pat.1, pat.2] <- "complex"  
                 }else{
-                  top.class.matrix[pat.1, pat.2] <- "multiAnc"  
+                  top.class.matrix[pat.1, pat.2] <- "multiAnc"
                 }
               } else {
                 top.class.matrix[pat.1, pat.2] <- "complex"
@@ -841,20 +892,10 @@ classify <- function(ptree, allow.mt = F, n.mt=Inf, p.mt= 2, relaxed.ancestry = 
               top.class.matrix[pat.1, pat.2] <- "desc"
             } else {
               if(allow.mt){
-                if(p.mt<=1){
-                  if(p.21 >=p.mt){
-                    top.class.matrix[pat.1, pat.2] <- "complex"
-                  }else{
-                    top.class.matrix[pat.1, pat.2] <- "multiDesc"  
-                  }
-                }else if(n.mt<=1e5){
-                  if(count.21>=n.mt){
-                    top.class.matrix[pat.1, pat.2] <- "complex"
-                  }else{
-                    top.class.matrix[pat.1, pat.2] <- "multiDesc"  
-                  }
+                if(tips.mrca.1==tips.mrca.2){
+                  top.class.matrix[pat.1, pat.2] <- "complex"  
                 }else{
-                  top.class.matrix[pat.1, pat.2] <- "multiDesc"  
+                  top.class.matrix[pat.1, pat.2] <- "multiDesc"
                 }
               } else {
                 top.class.matrix[pat.1, pat.2] <- "complex"
@@ -881,13 +922,6 @@ classify <- function(ptree, allow.mt = F, n.mt=Inf, p.mt= 2, relaxed.ancestry = 
           }
         }
       }
-      if(verbose){
-        cat('host 1: ',pat.1.id,' host.2: ', pat.2.id,'\n',
-            'nodes.1: ',length(nodes.1),' nodes.2: ', length(nodes.2),'\n',
-            'count.12: ',count.12,' count.21: ',count.21,'\n',
-            'p.12: ',p.12, ' p.21: ',p.21,'\n')
-      }
-      
     }
   }
   
