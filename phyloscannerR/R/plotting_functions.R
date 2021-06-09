99e678a0c040fbede868d0dbe921a1c236401196
XiaoyueXI <xiaoyue.xi94@gmail.com>
2021-06-09 12:40:29 +0100

    minor change in plot

--- phyloscannerR/R/plotting_functions.R
+++ phyloscannerR/R/plotting_functions.R
@@ -508,7 +508,9 @@ produce.pairwise.graphs2 <- function(	ptrees,
 	# set min patristic distance for plotting, make factors, etc
 	pair.data <- pair.data %>%
 			mutate(	patristic.distance:= pmax(1e-3,patristic.distance), 
-					basic.topology:= factor(basic.topology, levels=c("ancestral","descendant","intermingled",'sibling','other')),
+					# basic.topology:= factor(basic.topology, levels=c("ancestral","descendant","intermingled",'sibling','other')),
+			                # switch to another definition
+			                basic.topology:= factor(basic.topology),
 					panel:= paste0(host.1,'\n->\n',host.2),
 					y:= 1e-3)			
 	# make plot
