985ce8e55fdac783edf01efc36d83b5f917c60d4
Mélodie MONOD <monod.melodie@gmail.com>
2021-11-18 10:07:20 +0000

    save env

--- phyloflows/inst/code_stan.R
+++ phyloflows/inst/code_stan.R
@@ -10,8 +10,12 @@ if(0){
 }
 
 DEBUG <- F
+stan_model <- '211115'
+lab <- stan_model
+
 path.to.stan.data <- file.path(outdir, 'input.rda')
-path.to.stan.model <- file.path(indir, 'stan_models', 'gp_211115.stan')
+path.to.stan.model <- file.path(indir, 'stan_models', paste0('gp_', stan_model, '.stan'))
+outdir.lab <- file.path(outdir, lab); dir.create(outdir.lab)
 
 load(path.to.stan.data)
 source(file.path(indir, 'functions', 'stan_utils.R'))
@@ -19,6 +23,11 @@ source(file.path(indir, 'functions', 'stan_utils.R'))
 # stan data
 stan_data <- ageanalysis(outdir = outdir)
 stan_data <- add_2D_splines_stan_data(stan_data, spline_degree = 3, n_knots_rows = 15, n_knots_columns = 15, AGES = floor(sort(unique(stan_data$x[,1]))))
+
+# save image before running Stan
+tmp <- names(.GlobalEnv)
+tmp <- tmp[!grepl('^.__|^\\.|^model$',tmp)]
+save(list=tmp, file=file.path(outdir.lab, paste0("stanin_",lab,".RData")) )
 
 # run stan model
 options(mc.cores = parallel::detectCores())
@@ -36,7 +45,7 @@ if(DEBUG){
 sum = summary(fit)
 sum$summary[which(sum$summary[,9] < 100),]
 
-save(fit,file = file.path(outdir, 'gp_211115.rda'))
+save(fit,file = file.path(outdir.lab, 'gp_211115.rda'))
 
 
 
