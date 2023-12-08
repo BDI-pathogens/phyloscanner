390fdf304ad1ca25e7c4c985812691f79adf24d9
susannah-b <susannahblundell98@gmail.com>
2023-12-08 14:29:13 +0000

    DefaultVariables.py edit

--- tools/DefaultVariables.py
+++ tools/DefaultVariables.py
@@ -1,3 +1,4 @@
+#Default options for phyloscanner_make_trees.py and CalculateTReeSizeINGenomeWindows.py
 RAxMLdefaultOptions = "--model GTR+F+R6 --seed 1"
 RAxMLOlddefaultOptions = "-m GTRCAT -p 1 --no-seq-check"
 IQtreedefaultOptions = "-m GTR+F+R6 -seed 1"
